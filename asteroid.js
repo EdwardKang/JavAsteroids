c5379fb69685404acbe47810c026d3fa62e06b8a
Edward Kang <eyk23@cornell.edu>
2014-01-10 20:34:57 -0500

    added asteroids that break up into smaller asteroids

--- asteroid.js
+++ asteroid.js
@@ -1,23 +1,30 @@
 (function(root) {
   var Asteroids = root.Asteroids = (root.Asteroids || {});
-  var Asteroid = Asteroids.Asteroid = function Asteroid(startPos, vel, game) {
-    Asteroids.MovingObject.call(this, startPos, vel, Asteroid.RADIUS, Asteroid.COLOR, game)
+  var Asteroid = Asteroids.Asteroid = function Asteroid(startPos, vel, radius, game) {
+    Asteroids.MovingObject.call(this, startPos, vel, radius, Asteroid.COLOR, game)
   };
 
   Asteroid.inherits(Asteroids.MovingObject);
   Asteroid.COLOR = 'black';
   Asteroid.RADIUS = 10;
 
-  Asteroid.randomAsteroid = function(dimX, dimY, game) {
+  Asteroid.randomAsteroid = function(dimX, dimY, radius, game) {
     var posX = Math.random() * dimX;
     var posY = Math.random() * dimY;
     var startPos = [posX, posY];
-    return new Asteroid(startPos, randomVel(), game);
+    return new Asteroid(startPos, randomVel(), radius, game);
   };
+	
+	Asteroid.placedAsteroid = function(startX, startY, radius, startVel, game) {
+		var startPos = [startX, startY];
+		var tempVel = randomVel();
+		var newVel = [(startVel[0] + tempVel[0]), (startVel[1] + tempVel[1])]
+		return new Asteroid(startPos, newVel, radius, game);
+	};
 
   var randomVel = function() {
-    var velX = Math.random() * 4 - 2;
-    var velY = Math.random() * 4 - 2;
+    var velX = Math.random() * 2 - 1;
+    var velY = Math.random() * 2 - 1;
     return [velX, velY];
   };
 })(this);
--- game.js
+++ game.js
@@ -1,28 +1,35 @@
 (function(root) {
 
   var Asteroids = root.Asteroids = (root.Asteroids || {});
-  var Game = Asteroids.Game = function(ctx) {
+	
+	var Game = Asteroids.Game = function(ctx) {
     this.ctx = ctx;
+    this.reset();
+  };
+	
+  Game.DIM_X = 600;
+  Game.DIM_Y = 600;
+  Game.FPS = 15;
+	
+	Game.prototype.reset = function() {
     this.timerId = null;
-    this.asteroids = this.addAsteroids(10);
+    this.asteroids = this.addAsteroids(6, 36);
     this.ship = new Asteroids.Ship([(Game.DIM_X / 2), (Game.DIM_Y / 2)], [0,0], this, Math.PI);
     this.bullets = [];
 		this.fireBullet = false;
 		this.lastCreatedBullet = 0;
 		this.gameOver = false;
-  };
+		
+		this.start();
+	};
+	
+  Game.prototype.start = function() {
+    var game = this;
+    game.bindKeyHandlers();
 
-  Game.DIM_X = 600;
-  Game.DIM_Y = 600;
-  Game.FPS = 15;
-
-  Game.prototype.addAsteroids = function(num) {
-    var asteroids = [];
-
-    _.times(num, function() {
-      asteroids.push(Asteroids.Asteroid.randomAsteroid(Game.DIM_X, Game.DIM_Y, this));
-    });
-    return asteroids;
+    game.timerId = window.setInterval(function() {
+      game.step();
+    }, Game.FPS);
   };
 
   Game.prototype.draw = function() {
@@ -59,30 +66,17 @@
     this.draw();
     this.checkCollisions();
 		
+		if (this.asteroids.length === 0) {
+			this.win();
+		}
+		
 		var that = this;
 		
 		key('space', function(){ 
 			if (that.gameOver) {
-		    that.timerId = null;
-		    that.asteroids = that.addAsteroids(10);
-		    that.ship = new Asteroids.Ship([(Game.DIM_X / 2), (Game.DIM_Y / 2)], [0,0], that, Math.PI);
-		    that.bullets = [];
-				that.fireBullet = false;
-				that.lastCreatedBullet = 0;
-				that.gameOver = false;
-				
-				that.start();
-			}
+		    that.reset();
+			};
 		});
-  };
-
-  Game.prototype.start = function() {
-    var game = this;
-    game.bindKeyHandlers();
-
-    game.timerId = window.setInterval(function() {
-      game.step();
-    }, Game.FPS);
   };
 
   Game.prototype.checkCollisions = function() {
@@ -94,16 +88,40 @@
       }
     });
   };
+	
+  Game.prototype.addAsteroids = function(num, radius, startPos, vel) {
+		var asteroids = [];
+
+    _.times(num, function() {
+			if (typeof startPos === 'undefined') {
+			  asteroids.push(Asteroids.Asteroid.randomAsteroid(Game.DIM_X, Game.DIM_Y, radius, this));
+		  } else {
+		  	asteroids.push(Asteroids.Asteroid.placedAsteroid(startPos[0], startPos[1], radius, vel, this));
+		  }
+    });
+		
+		return asteroids
+  };
 
   Game.prototype.removeAsteroid = function(hitAsteroid) {
     var game = this;
+		var newAsteroids = [];
 
     _.each(game.asteroids, function(asteroid, idx) {
       if (hitAsteroid === asteroid) {
+				if (asteroid.radius === 36){
+					newAsteroids = game.addAsteroids(2, 24, asteroid.pos, asteroid.vel);
+				} else if (asteroid.radius === 24) {
+					newAsteroids = game.addAsteroids(2, 12, asteroid.pos, asteroid.vel);
+				} else if (asteroid.radius === 12) {
+					newAsteroids = game.addAsteroids(2, 6, asteroid.pos, asteroid.vel);
+				}
+				
         game.asteroids[idx] = false;
       }
     });
     game.asteroids = _.compact(game.asteroids);
+		game.asteroids = game.asteroids.concat(newAsteroids);
   };
 
   Game.prototype.removeBullet = function(usedBullet) {
@@ -126,6 +144,16 @@
 		ctx.fillText("press space to continue", 230,265)
 		this.gameOver = true;
   };
+	
+	Game.prototype.win = function() {
+    window.clearInterval(this.timerId);
+		ctx.fillStyle = "black";
+		ctx.font = "20pt Arial";
+		ctx.fillText("You Win!!", 240,250);
+		ctx.font = "10pt Arial";
+		ctx.fillText("press space to restart", 235,265)
+		this.gameOver = true;
+	};
 
   Game.prototype.bindKeyHandlers = function() {
     var game = this;
